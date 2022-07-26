dd92cbb5f6a3ba3131f449b5a3b69ad6df758d25
Ankit Kumar <animusk7765@gmail.com>
2022-07-26 21:46:47 +0530

    making more customize

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { ethers } from "ethers";
 import contractAbi from './artifacts/contracts/Remote_Purchase.sol/Remote_Purchase.json';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-const contractAddress = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
 	// console.log(contractAbi.abi);
 	const [buyer, setSeller] = useState(null);
-	const [sellerBalance, setSellerBalance] = useState(null);
-	const [contractBalance, setContractBalance] = useState(null);
+    const [purchase_Amount,setpurchase_Amount]=useState(null);
 
 
 	//const signer = provider.getSigner()
@@ -38,10 +38,12 @@ function App() {
 		}
 	}
 	async function ConfirmPurchase() {
+		
 		if (typeof window.ethereum !== "undefined") {
 			await requestAccount();
 		}
-		const lockedAmount = ethers.utils.parseEther("4");
+		console.log(purchase_Amount)
+		const lockedAmount = ethers.utils.parseEther(purchase_Amount);
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
 		const signer = provider.getSigner();
 		const contract = new ethers.Contract(contractAddress, contractAbi.abi, signer);
@@ -57,14 +59,14 @@ function App() {
 		});
 		await transaction_ConfirmPurchase.wait();
 		const contractBalance = await contract.getBalanceof();
-		console.log("contract address:", contractBalance.toString());
+		console.log("contract Balance:", contractBalance.toString());
 
 
 
 	}
 	async function getFunction1() {
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
-		
+
 		const contract = new ethers.Contract(contractAddress, contractAbi.abi, provider);
 		const buyer = await contract.buyer();
 		console.log("Buyer Address :", buyer);
@@ -74,7 +76,7 @@ function App() {
 	}
 	async function confirmRecieved() {
 		const provider = new ethers.providers.Web3Provider(window.ethereum);
-		const signer=provider.getSigner();
+		const signer = provider.getSigner();
 		const contract = new ethers.Contract(contractAddress, contractAbi.abi, signer);
 		const transaction_confirmRecieved = await contract.confirmRecieved();
 		await transaction_confirmRecieved.wait();
@@ -83,20 +85,28 @@ function App() {
 
 	}
 	async function paySeller() {
-		const provider=new ethers.providers.Web3Provider(window.ethereum);
-		const signer=provider.getSigner();
-		const contract= new ethers.Contract(contractAddress,contractAbi.abi,signer);
-		const transaction_paySeller=await contract.paySeller();
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const signer = provider.getSigner();
+		const contract = new ethers.Contract(contractAddress, contractAbi.abi, signer);
+		const transaction_paySeller = await contract.paySeller();
 		await transaction_paySeller.wait();
 	}
-	useEffect(() => {
-		requestAccount();
-		getfunction();
-		ConfirmPurchase();
-		getFunction1();
-		confirmRecieved();
-		paySeller();
-	}, []);
+
+	async function abort() {
+		const provider = new ethers.providers.Web3Provider(window.ethereum);
+		const signer = provider.getSigner();
+		const contract = new ethers.Contract(contractAddress, contractAbi.abi, signer);
+		const transaction_abort = await contract.abort();
+		await transaction_abort.wait();
+	}
+	// useEffect(() => {
+	// 	requestAccount();
+	// 	getfunction();
+	// 	ConfirmPurchase();
+	// 	getFunction1();
+	// 	confirmRecieved();
+	// 	paySeller();
+	// }, []);
 	// requestAccoun();ConfirmPurchase
 	// ConfirmPurchase();
 
@@ -107,12 +117,21 @@ function App() {
 				<br />
 				Buyer Account :{buyer}
 				<br />
-				Seller Balance :{sellerBalance}
-				<br />
-
-
-
 			</h1>
+			<Button onClick={requestAccount}>Connect_To_Metamask</Button>
+			<br />
+			<Button onClick={getfunction} variant="success">Get_function</Button>
+			<br />
+			<input type="text" onChange={(event)=>setpurchase_Amount(event.target.value)}></input>
+			<Button variant="secondary" onClick={ConfirmPurchase}>ConfirmPurchase</Button>
+			<br />
+			<Button variant="danger" onClick={getFunction1}>getFunction1</Button>
+			<br />
+			<Button variant="warning" onClick={confirmRecieved}>confirmRecieved</Button>
+			<br />
+			<Button variant="info" onClick={paySeller}>PaySeller</Button>
+			<br />
+			<Button variant="danger" onClick={abort}>Abort</Button>
 		</div>
 	);
 }
